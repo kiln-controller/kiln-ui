4320cef08d208a5701efce23e672b5d10c070c56
Wilmar den Ouden <info@wilmardenouden.nl>
2024-01-05 00:16:49 +0100

    fix: build assets without postfix hash
    Signed-off-by: wilmarguida <w.denouden@guida.nl>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    cssCodeSplit: false
+    // https://github.com/vitejs/vite/issues/378#issuecomment-768816653
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 })
